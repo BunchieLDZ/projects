b1a40d2da06af2826117f43a9534ea0390888317
Bunchie <gavroche.ldz@gmail.com>
2020-01-07 07:37:58 +0100

    Changes to pixel collision detection

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ class Game {
         this.defeat = false;
         this.score = 0;
         this.uiobjects = [];
-        this.playerobject = new Player(this.game_canvas.width / 2, this.game_canvas.height / 2, 0, 0, 0.5,  128, 64, "res/ufo.png", 120);
+        this.playerobject = new Player(this.game_canvas.width / 2, this.game_canvas.height / 2, 0, 0, 0.5, 112, 96, "res/ufo.png", 120);
         this.scene = new Scene("res/background.jpg", 0, 0, 0, 0, 100, 800, 400);
         this.asteroids_generator = new AsteroidGenerator(500, 1200, 33, -10, this.game_canvas.height, this.game_canvas.width);
         console.log("Generator: ", this.asteroids_generator);
@@ -137,34 +137,41 @@ class Game {
         if(obj1.bbx1 < obj2.bbx1) {
             clx = obj2.bbx1;
         }
-        else {
+        if(obj1.bbx1 > obj2.bbx1) {
+        //else {
             clx = obj1.bbx1;
         }
         if(obj1.bby1 < obj2.bby1) {
             cly = obj2.bby1;
         }
-        else {
+        if(obj1.bby1 > obj2.bby1) {
+        //else {
             cly = obj1.bby1;
         }
         if(obj1.bbx2 < obj2.bbx2) {
             cbx = obj1.bbx2;
         }
-        else {
+        if(obj1.bbx2 > obj2.bbx2) {
+        //else {
             cbx = obj2.bbx2;
         }
         if(obj1.bby2 < obj2.bby2) {
             cby = obj1.bby2;
         }
-        else {
+        if(obj1.bby2 > obj2.bby2) {
+        //else {
             cby = obj2.bby2;
         }
+    
+        //console.log("bbx1: ", obj1.bbx1, obj2.bbx1);
+        //console.log("Ustalone wartosci: ", clx, cly, cbx, cby);
     
         coordinates[0] = clx;
         coordinates[1] = cly;
         coordinates[2] = cbx;
         coordinates[3] = cby;
     
-        console.log("WSPOLRZEDNE OBSZARU WSPOLNEGO: ", clx, cly, cbx, cby);
+        //console.log("WSPOLRZEDNE OBSZARU WSPOLNEGO: ", clx, cly, cbx, cby);
     
         return coordinates;
     
@@ -201,40 +208,46 @@ class Game {
     //console.log(this.playerobject.health);
     }
     pixel_collision(obj1, obj2) { // dokladniejsza kolizja per pixel
-    // wyznaczamy 4 koordynaty dwoch punktow ktore stanowia krance nachodzacego prostokata
-    var clx = this.overlapping_area(obj1, obj2)[0]; 
-    var cly = this.overlapping_area(obj1, obj2)[1];
-    var cbx = this.overlapping_area(obj1, obj2)[2];
-    var cby = this.overlapping_area(obj1, obj2)[3];
-    console.log(clx, cly, cbx, cby);
-    console.log(this.ictx);
-    // jesli szerokosc i wysokosc tego prostokata jest wieksza/rowna 1
-    if((cbx-clx) >= 1 && (cby - cly) >= 1) {
-    this.ictx.clearRect(clx, cly, cbx - clx, cby - cly); // czyszczenie tymczasowej kanwy
-    obj1.draw(this.ictx); // rysowanie pierwszego obiektu
-    var imgdata1 = this.ictx.getImageData(clx, cly, cbx - clx, cby - cly); // wyciagniecie imgdata tego obiektu
-    this.ictx.clearRect(clx, cly, cbx - clx, cby - cly); // to samo dla drugiego obiektu
-    obj2.draw(this.ictx);
-    var imgdata2 = this.ictx.getImageData(clx, cly, cbx - clx, cby - cly);
-
-    console.log(imgdata1);
-
-    for(var i = 3; i < imgdata1.data.length; i+=4) // sprawdzanie kanalu alpha obiektow
-        {
-        if(!imgdata1.data[i] && !imgdata2.data[i]) // jesli pixele sa przezroczyste to sprawdzamy dalej
+        this.ictx.clearRect(0, 0, this.ictx.width, this.ictx.height);
+        // wyznaczamy 4 koordynaty dwoch punktow ktore stanowia krance nachodzacego prostokata
+        var clx = this.overlapping_area(obj1, obj2)[0]; 
+        var cly = this.overlapping_area(obj1, obj2)[1];
+        var cbx = this.overlapping_area(obj1, obj2)[2];
+        var cby = this.overlapping_area(obj1, obj2)[3];
+        console.log("Koordynaty obszaru wspolnego: ", clx, cly, cbx, cby);
+        console.log("Wielkosc obszaru wspolnego: ", (cbx - clx) * (cby - cly), " px");
+        //console.log(ictx);
+        // jesli szerokosc i wysokosc tego prostokata jest wieksza/rowna 1
+        if((cbx-clx) >= 1 && (cby - cly) >= 1) { // jesli wielkosc obszaru wspolnego jest wieksza od 1 to sprawdzamy pixel po pixelu
+        this.ictx.clearRect(clx, cly, cbx - clx, cby - cly); // czyszczenie tymczasowej kanwy
+        obj1.draw(this.ictx); // rysowanie pierwszego obiektu
+        var imgdata1 = this.ictx.getImageData(clx, cly, cbx - clx, cby - cly); // wyciagniecie imgdata tego obiektu
+        this.ictx.clearRect(clx, cly, cbx - clx, cby - cly); // to samo dla drugiego obiektu
+        obj2.draw(this.ictx);
+        var imgdata2 = this.ictx.getImageData(clx, cly, cbx - clx, cby - cly);
+        var resolution = 32; // dla poprawy performesu sprawdzamy tylko co ktorys pixel
+        //console.log(imgdata1);
+        for (var j = 0; j < imgdata1.data.length; j+= resolution) {
+            for (var i = 3; i < imgdata1.data.length; i += 4) // sprawdzanie kanalu alpha obiektow
             {
-           continue;
-            }
-        else { // jesli nie jest to mamy do czynienia z kolizja
-            console.log("KOLIZJA!");
-            this.playerobject.on_hit()
-            /*context.fillStyle = "black";
-            context.font = "30px Arial";
-            context.fillText("Kolizja!", 300, 50);*/
+               // console.log("Wielkosc tablicy 1:", imgdata1.data.length);
+               // console.log("Wielkosc tablicy 2:", imgdata2.data.length);
+                //if(!imgdata1.data[i] && !imgdata2.data[i]) // jesli pixele sa przezroczyste to sprawdzamy dalej
+                if (imgdata1.data[i] == 0 || (imgdata2.data[i] == 0)) {
+                    continue;
+                }
+                else { // jesli nie jest to mamy do czynienia z kolizja
+                    console.log("Numer pixela: ", i);
+                    console.log("Nieprzezroczysty pixel", imgdata1.data[i], imgdata2.data[i]);
+                    return true;
+                    }
                 }
             }
+        //else { // jesli wielkosc obszaru wspolnego jest mniejsza w trakcie box collision to obiekty nachodza na siebie bezposrednio
+          //  return true;
+        //}
         }
-    }   
+    }
     on_player_hit() {
         this.game_ctx.font = "30px Arial";
         this.game_ctx.fillStyle = "white";
@@ -272,7 +285,9 @@ class Game {
                 this.asteroids_generator.asteroids[i].draw(this.game_ctx);
                 //this.box_collision(this.asteroids_generator.asteroids[i], this.playerobject);
                 if(this.box_collision(this.playerobject, this.asteroids_generator.asteroids[i]) && this.playerobject.is_immune == false){
-                    this.pixel_collision(this.playerobject, this.asteroids_generator.asteroids[i]);
+                    if(this.pixel_collision(this.playerobject, this.asteroids_generator.asteroids[i])) {
+                        this.playerobject.on_hit();
+                    }
                 }
             }
             if(this.asteroids_generator.asteroids[i].x < 0) {
